23c2d10cd5524dc22e40373625e5ce4c497a4f1f
ekonboy <gabrielrese@gmail.com>
2024-10-02 15:44:20 +0200

    crear store de pinia para almacenar los datos de registro

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,25 +1,43 @@
 <template>
     <div class="sidebar">
-    <h2>Barra lateral</h2>
-    <ul>
-    <li><router-link to="/contador">Contador</router-link></li>
-    <li><router-link to="/lista-de-tareas">Lista de tareas</router-link></li>
-    <li><router-link to="/registrar">Registrar</router-link></li>
-    </ul></div>
-    
-    
-    
+        <h2>Bienvenido</h2>
+        <h3>Nombre: {{ nombre }}</h3>
+        <h3>Email: {{ email }}</h3>
+        <ul>
+            <li><router-link to="/contador">Contador</router-link></li>
+            <li><router-link to="/lista-de-tareas">Lista de tareas</router-link></li>
+            <li><router-link to="/registrar">Registrar</router-link></li>
+        </ul>
+    </div>
 </template>
-    
-    
-    
-    <script setup>
-    
-    </script>
-    
-    
-    <style>
-  
-    .sidebar {width: 200px;background-color: aquamarine;padding: 20px;}
-    
-    </style>
+
+<script setup>
+import { useRegistrarStore } from '@/modules/contador/registro/stores/registrarStore';
+import { ref, watch } from 'vue';
+
+const RegistrarStore = useRegistrarStore();
+const nombre = ref(RegistrarStore.nombre);
+const email = ref(RegistrarStore.email);
+
+
+
+// Si `nombre` es un valor reactivo dentro de `RegistrarStore`, también puedes hacer que el `watch` observe directamente el valor dentro de la tienda:
+watch(() => RegistrarStore.nombre, (newValue) => {
+    //console.log(`El nombre en la tienda ha cambiado a ${newValue}`);
+    nombre.value = newValue;
+});
+
+watch(() => RegistrarStore.email, (newValue) => {
+    email.value = newValue;
+});
+
+
+</script>
+
+<style>
+.sidebar {
+    width: 200px;
+    background-color: aquamarine;
+    padding: 20px;
+}
+</style>
--- src/modules/contador/registro/views/RegistrarView.vue
+++ src/modules/contador/registro/views/RegistrarView.vue
@@ -5,13 +5,13 @@
             
             <div class="form">
             <label for="nombre">Nombre:</label> 
-            <Field type="text" name="nombre" id="nombre" placeholder="ingresa tu nombre" />
+            <Field v-model="nombre" type="text" name="nombre" id="nombre" placeholder="ingresa tu nombre" />
             <ErrorMessage name="nombre"></ErrorMessage>
             </div>
            
             <div class="form">
             <label for="email">Email:</label> 
-            <Field type="email" name="email"  id="email" placeholder="ingresa tu mail" />
+            <Field v-model="email"  type="email" name="email"  id="email" placeholder="ingresa tu mail" />
             <ErrorMessage name="email"></ErrorMessage>
             <div class="form"> 
             <button type="submit">Registrar</button></div>
@@ -21,13 +21,21 @@
 </template>
 <script setup>
 import { Form, Field, ErrorMessage } from 'vee-validate';
-
 import {schema} from '../schemas/validationSchema';
+import {useRegistrarStore} from '../stores/registrarStore';
+import {ref} from 'vue';
+
+const RegistrarStore = useRegistrarStore();
+const nombre = ref('');
+const email = ref('');
+
 
 const onSubmit = () => {
+    RegistrarStore.guardarRegistro(nombre.value, email.value)
     console.log('se ha enviado correctamente');
+
 }
 </script>
 <style scoped>
-.form {margin-bottom: 20px;}
+.form {margin-bottom: 20px;} 
 </style>
